3aafcb27743f9535b5f9335d8a8d26534e03a67b
katherinehotexas <89663837+katherinehotexas@users.noreply.github.com>
2022-11-24 16:06:24 -0800

    First commit

---
+++ logic.js
@@ -0,0 +1,8 @@
+function revealPlay() {
+    var x = document.getElementById("countryDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
